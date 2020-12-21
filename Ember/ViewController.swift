330eb3f82e7389ceee4bd9b7e2253a7ca0667071
Vishal Singh <edsingh@ucdavis.edu>
2020-12-20 20:28:49 -0800

    Initial Commit

---
+++ Ember/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ember
+//
+//  Created by Vishal Singh on 12/20/20.
+//  Copyright © 2020 Vishal Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
